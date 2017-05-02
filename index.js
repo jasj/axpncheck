ef8c22fd96da6fdb1f5b2b425031f2e11c657003
Juan Andres Segreda Johanning <andres@segreda.com>
2017-05-02 00:35:20 -0600

    add cross domain

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    next();
+};
+
+
+
+ 
+app.use(allowCrossDomain);
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
